3bef2a2535199c23db24e46054d58c1411f4ca57
Ashvini379 <ashii32mahajan@gmail.com>
2022-02-17 06:42:43 +0530

    Add project files.

---
+++ AzureSqlDbApp/Services/CourseService.cs
@@ -0,0 +1,56 @@
+﻿using AzureSqlDbApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureSqlDbApp.Services
+{
+    public class CourseService
+    {
+        //private static string db_source = "appdbserverashvini.database.windows.net";
+        //private static string db_user = "ashvini";
+        //private static string db_password = "Tanvi@2013";
+        //private static string db_database = "appdb";
+        private SqlConnection GetConnection(string connectionstring)
+        {
+            // Here we are creating the SQL connection
+            //var _builder = new SqlConnectionStringBuilder();
+            //_builder.DataSource = db_source;
+            //_builder.UserID = db_user;
+            //_builder.Password = db_password;
+            //_builder.InitialCatalog = db_database;
+            return new SqlConnection(connectionstring);
+        }
+
+        public IEnumerable<Course> GetCourses(string connectionstring)
+        {
+            List<Course> _lst = new List<Course>();
+            string _statement = "SELECT CourseID,CourseName,rating from Course";
+            SqlConnection _connection = GetConnection(connectionstring);
+            // Let's open the connection
+            _connection.Open();
+            // We then construct the statement of getting the data from the Course table
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+            // Using the SqlDataReader class , we will read all the data from the Course table
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Course _course = new Course()
+                    {
+                        CourseId = _reader.GetInt32(0),
+                        CourseName = _reader.GetString(1),
+                        Rating = _reader.GetDecimal(2)
+                    };
+
+                    _lst.Add(_course);
+                }
+            }
+            _connection.Close();
+            return _lst;
+        }
+
+    }
+}
